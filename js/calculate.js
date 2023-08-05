0703fd67782919bcedb3ac68b8cc9d4548edb847
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2023-08-05 16:16:46 +0600

    claculate part done

--- js/calculate.js
+++ js/calculate.js
@@ -1,3 +1,28 @@
-document.getElementById('calculate-button').addEventListener('click',function(){
-    console.log('c');
-})
+function inputFieldSetByID(id) {
+  let fieldInput = document.getElementById(id);
+  let fieldString = fieldInput.value;
+  let fieldValue = Number(fieldString);
+  fieldInput.value=''
+  return fieldValue;
+}
+function elementSetTextById(id,value){
+    const totalExpenses=document.getElementById(id);
+    totalExpenses.innerText=value;
+
+}
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    const incomeInputField = inputFieldSetByID("income-input");
+    const foodInputField = inputFieldSetByID("food-input");
+    const rentInputField = inputFieldSetByID("rent-input");
+    const clothesInputField = inputFieldSetByID("clothes-input");
+
+    const expense=foodInputField+rentInputField+clothesInputField;
+    elementSetTextById('total-expenses',expense);
+    let balance=incomeInputField-expense;
+    elementSetTextById('balance',balance)
+  
+
+    
+  });
